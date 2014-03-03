094802e650d9c115cfdbfe2032c2834fe859d843
rbennett485 <rbennett485@gmail.com>
2014-03-03 17:00:10 +0000

    now decodes bnz

--- SML/src/sml/Translator.java
+++ SML/src/sml/Translator.java
@@ -77,6 +77,7 @@ public class Translator {
 		int s2;
 		int r;
 		int x;
+		String l2;
 
 		if (line.equals(""))
 			return null;
@@ -100,6 +101,10 @@ public class Translator {
 		case "out":
 			s1 = scanInt();
 			return new OutInstruction(label, s1);
+		case "bnz":
+			s1 = scanInt();
+			l2 = scan();
+			return new BnzInstruction(label, s1, l2);
 		}
 
 		// You will have to write code here for the other instructions.
