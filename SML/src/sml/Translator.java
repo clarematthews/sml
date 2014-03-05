090e150860564b463ffa43ddfbfd2cfa2d9aea12
Clare Matthews <clarematthews@gmail.com>
2014-03-05 21:48:43 +0000

    Added div case to Translator.

--- SML/src/sml/Translator.java
+++ SML/src/sml/Translator.java
@@ -91,8 +91,8 @@ public class Translator {
 			return new AddInstruction(label, r, s1, s2);
 		case "lin":
 			r = scanInt();
-			s1 = scanInt();
-			return new LinInstruction(label, r, s1);
+			x = scanInt();
+			return new LinInstruction(label, r, x);
 		case "sub":
 			r = scanInt();
 			s1 = scanInt();
@@ -110,9 +110,12 @@ public class Translator {
 			s1 = scanInt();
 			s2 = scanInt();
 			return new MulInstruction(label, r, s1, s2);
+		case "div":
+			r = scanInt();
+			s1 = scanInt();
+			s2 = scanInt();
+			return new DivInstruction(label, r, s1, s2);
 		}
-
-		// You will have to write code here for the other instructions.
 
 		return null;
 	}
