eb427b44a367295b0fdd2e557ca6a119dc7e38e6
Clare Matthews <clarematthews@gmail.com>
2014-04-09 13:33:07 +0100

    Added division by zero check to DivInstruction. Added properties file.

--- SML/src/sml/DivInstruction.java
+++ SML/src/sml/DivInstruction.java
@@ -33,7 +33,12 @@ public class DivInstruction extends Instruction {
 	public void execute(Machine m) {
 		int value1 = m.getRegisters().getRegister(op1);
 		int value2 = m.getRegisters().getRegister(op2);
-		m.getRegisters().setRegister(result, value1/value2);
+		try {
+			m.getRegisters().setRegister(result, value1/value2);
+		}
+		catch(ArithmeticException ex) {
+			System.err.println("Register " + result + " not updated. " + ex.getMessage());
+		}
 	}
 	
 	@Override
